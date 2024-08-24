fb86b66fc9af853841a3162d305397be5e730fc6
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-08-23 20:24:28 -0700

    post detail page formatted with extracting data from markdown file

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,23 +3,15 @@ import PostHeader from "./post-header";
 import classes from './post-content.module.css'
 import ReactMarkdown from 'react-markdown'
 
-const DUMMY_POST =
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    date: "2023-10-15",
-    content: "# This is NextJS Project",
-  }
 
 
-export default function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+export default function PostContent({post}) {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,33 @@
 import React from 'react'
 import PostContent from '../../components/posts/post-detail/post-content'
+import { getAllFileNames, getPostData } from '../../lib/post-util'
 
-export default function PostDetailPage() {
+export default function PostDetailPage({postData}) {
   return (
-    <PostContent />
+    <PostContent post={postData}/>
   )
+}
+
+export function getStaticProps(context){
+    
+    const { params } = context
+    const { slug } = params
+
+    const postData = getPostData(slug)
+    return {
+        props: {
+            postData : postData
+        },
+        revalidate: 1800
+    }
+}
+
+export function getStaticPaths(){
+    const fileNames = getAllFileNames()
+
+    const allSlugs = fileNames.map(fileName => fileName.replace(/\.md$/, ''))
+    return {
+        paths: allSlugs.map(slug => ({params: {slug: slug}})),
+        fallback: false
+    }
 }
